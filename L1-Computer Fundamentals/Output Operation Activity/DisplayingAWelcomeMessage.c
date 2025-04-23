6c208cb55482c92eefe1d44d417b3f0b2bd56dc3
jed <21alcala.j.i@gmail.com>
2025-04-23 15:32:38 +0000

    DisplayingAWelcomeMessage.c

---
+++ L1-Computer Fundamentals/Output Operation Activity/DisplayingAWelcomeMessage.c
@@ -0,0 +1,14 @@
+/**
+ * @file DisplayingAWelcomeMessage.c
+ * @brief A simple C program that greets the users by displaying the message "Hello, World!"
+ * 
+ * Computer : Hello, World! :)
+ * 
+ * @author jedi-766
+ * @date April 23, 2025
+ */
+ 
+ #include <stdio.h>
+ int main(){
+     printf("Hello, World!"); /*To great a user when run the code*/
+ }

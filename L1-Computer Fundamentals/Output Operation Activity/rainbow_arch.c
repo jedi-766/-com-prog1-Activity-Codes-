7fdaafdf207fbc43ea262f8d50be891a2e3fe226
jed <21alcala.j.i@gmail.com>
2025-04-23 15:29:19 +0000

    rainbow_arch.c

---
+++ L1-Computer Fundamentals/Output Operation Activity/rainbow_arch.c
@@ -0,0 +1,17 @@
+/**
+ * @file rainbow_arch.c
+ * @brief A simple C program to print a rainbow arch.
+ *
+ * This program prints a rainbow arch using basic string formatting.
+ * The colors are aligned to form an arch shape.
+ *
+ * @author jedi-766
+ * @date April 23,2025
+ */
+#include<stdio.h>
+int main(){
+    printf("            Green\n");
+    printf("      Yellow     Brown\n");
+    printf("  Orange             Indigo\n");
+    printf("Red                      Violet\n");
+}

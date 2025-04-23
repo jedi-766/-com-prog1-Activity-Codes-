2bf5246aac60a5f753cc5f37429ac84b28c2131f
jed <21alcala.j.i@gmail.com>
2025-04-23 15:31:05 +0000

    CODESymphony.c

---
+++ L1-Computer Fundamentals/Output Operation Activity/CODESymphony.c
@@ -0,0 +1,22 @@
+/**
+ * @file CODESymphony.c
+ * @brief A simple C program that showcases a pattern of the word "CODE"
+ * 
+ * CCCCC
+ * C
+ * C
+ * C
+ * CCCCC
+ * 
+ * @author jedi-766
+ * @date April 23, 2025
+ */
+ 
+ #include<stdio.h>
+int main(){
+    printf("CCCCC   OOOOO   DDDDD   EEEEE\n");
+    printf("C       O   O   D   D   E\n");
+    printf("C       O   O   D   D   EEE\n");
+    printf("C       O   O   D   D   E\n");
+    printf("CCCCC   OOOOO   DDDDD   EEEEE\n");
+}

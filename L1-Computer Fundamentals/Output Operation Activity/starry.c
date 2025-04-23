633a6ee39fcaa7a1aac65a2050450c5467647ddb
jed <21alcala.j.i@gmail.com>
2025-04-23 15:26:25 +0000

    starry.c

--- L1-Computer Fundamentals/Output Operation Activity/starry.c
+++ L1-Computer Fundamentals/Output Operation Activity/starry.c
@@ -1,8 +1,8 @@
 /**
  * @file starry.c
  * @brief A simple C program to generate a pattern of stars
- * @author Jed Ivan Alcala
- * @date February 26, 2025
+ * @author jedi-766
+ * @date April 23, 2025
  */
 #include<stdio.h>
 int main(){
